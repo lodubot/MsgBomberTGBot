7bfee090bffe6c526d056ce67cb34fb57cec6d8b
lodubot <jaihacku@gmail.com>
2025-09-10 21:08:06 +0530

    Update config_sample.py

--- config_sample.py
+++ config_sample.py
@@ -1,10 +1,10 @@
 import os
 class Config(object):
     # The bot-token which you can get from @Botfather
-    BOT_TOKEN = ""
+    BOT_TOKEN = "8471421689:AAFvERRQ6znLNP8JKv5Ou6FN7zGA-7uv-Yc"
     # There is no measure to limit who can use this bot, so add userids of users authorized to use this bot
-    AUTH_USERS = []
+    AUTH_USERS = [8100453801]
     # Add numbers below who shouldn't be bombed ever
     NO_BOMB_NUMS = []
     # Add userids below of users who should have sudo authority over bot, i.e., have no bombing limits
-    GOD_USERS = []
+    GOD_USERS = [8100453801]
